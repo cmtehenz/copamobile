6367e95837c2252465e22534d31ff13415a3a5da
Gustavo Costa <cmtehenz@gmail.com>
2022-11-06 10:59:35 -0300

    criado components e Find

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@
 
 import { AuthContextProvider } from './src/context/AuthContext';
 
-import { SignIn } from './src/screens/SignIn';
+import { Pools } from './src/screens/Pools';
 import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme';
@@ -20,7 +20,7 @@ export default function App() {
           translucent
         />
         {
-          fontsLoaded ? <SignIn /> : <Loading />
+          fontsLoaded ? <Pools /> : <Loading />
         }
       </AuthContextProvider>
     </NativeBaseProvider>
